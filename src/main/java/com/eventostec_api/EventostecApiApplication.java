ef1ba54a8a4b17c1fec74d491a403e9510833eb0
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-04-25 21:54:26 -0300

    first commit

---
+++ src/main/java/com/eventostec_api/EventostecApiApplication.java
@@ -0,0 +1,13 @@
+package com.eventostec_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventostecApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventostecApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventostec_api/EventostecApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventostec_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventostecApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
