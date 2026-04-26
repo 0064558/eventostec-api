25fc010d256b5851f29edc188c4602b160527b55
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-04-25 21:58:42 -0300

    feat: add repository interfaces for Address, Coupon, and Event

---
+++ src/main/java/com/eventostec_api/repositories/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.eventostec_api.repositories;
+
+import com.eventostec_api.domain.address.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
